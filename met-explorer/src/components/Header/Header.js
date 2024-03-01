445691fc3e231f2ce344a3561083f56ef854dabc
ÂIsel Castro <iselcq@gmail.com>
2024-03-01 11:11:58 -0600

    add JSX Header

--- met-explorer/src/components/Header/Header.js
+++ met-explorer/src/components/Header/Header.js
@@ -1,1 +1,20 @@
-function Header() {}
+import met_logo from "../../images/the_met_red.png";
+import "./Header.css";
+
+function Header() {
+  return (
+    <>
+      <header className="header">
+        <h2 className="header__title">The Met Xplorer</h2>
+        <img
+          src={met_logo}
+          alt="logotipo blanco de Around
+The U.S."
+          className="header__logo"
+        />
+      </header>
+    </>
+  );
+}
+
+export default Header;
